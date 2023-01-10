891fccb7daa2e55cb90d956974bcb7706c1600aa
QAlman <47560633+QAlman@users.noreply.github.com>
2023-01-10 22:33:13 +0300

    Update ApplicationManager.py

--- engine/ApplicationManager.py
+++ engine/ApplicationManager.py
@@ -5,8 +5,7 @@ from engine.API.APIBase import APIBase
 from engine.DriverInstance import DriverInstance
 from engine.WEB.AnyPage import AnyPage
 from engine.WEB.activity import Activity
-from engine.WEB.ecom.ecom_test import ecom_create
-from engine.WEB.ecom.ecom_steps import ecom_steps
+ 
 
 
 
@@ -21,6 +20,5 @@ class ApplicationManager:
         self.api_requests = ApiRequests(self)
         self.web_any_page = AnyPage(self)
         self.web_activity = Activity(self)
-        self.web_ecom = ecom_create(self)
         self.web_steps = ecom_steps(self)
 
